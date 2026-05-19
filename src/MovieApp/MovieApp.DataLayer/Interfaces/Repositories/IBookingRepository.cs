6483d06d82c79fc6b05049718b2293d9de552100
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-05-19 11:42:15 +0300

    done:3

---
+++ src/MovieApp/MovieApp.DataLayer/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,15 @@
+
+
+namespace MovieApp.DataLayer.Interfaces.Repositories;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MovieApp.DataLayer.Models;
+
+public interface IBookingRepository
+{
+    Task<IReadOnlyList<Booking>> GetByScreeningAsync(int screeningId, CancellationToken cancellationToken = default);
+
+    Task<bool> ReserveAsync(int screeningId, int userId, IReadOnlyList<(int Row, int Column)> seats, CancellationToken cancellationToken = default);
+}
