81a1bfd201dc43c0fa2ce9b79135dc9a89431037
waytoounoriginal <mihai.tira@yahoo.ro>
2026-05-23 14:53:37 +0300

    Changes variable name to prefix with '_'

--- src/MovieApp/MovieApp.WebApi/Endpoints/NotificationsEndpointsController.cs
+++ src/MovieApp/MovieApp.WebApi/Endpoints/NotificationsEndpointsController.cs
@@ -12,18 +12,18 @@ namespace MovieApp.WebApi.Endpoints;
 [Route("api/notifications")]
 public sealed class NotificationsEndpointsController : ControllerBase
 {
-    private readonly INotificationService notificationService;
+    private readonly INotificationService _notificationService;
 
     public NotificationsEndpointsController(INotificationService notificationService)
     {
-        this.notificationService = notificationService;
+        this._notificationService = notificationService;
     }
 
     [HttpGet("{userId:int}")]
     [RequireMatchingUser]
     public async Task<IActionResult> GetNotificationsByUser(int userId)
     {
-        var notifications = await this.notificationService.GetNotificationsByUserAsync(userId);
+        var notifications = await this._notificationService.GetNotificationsByUserAsync(userId);
         return Ok(notifications.Select(notification => notification.ToDto()));
     }
 
@@ -31,28 +31,28 @@ public sealed class NotificationsEndpointsController : ControllerBase
     [RequireMatchingUser]
     public async Task<IActionResult> GetUnreadNotifications(int userId)
     {
-        var unreadNotifications = await this.notificationService.GetUnreadNotificationsAsync(userId);
+        var unreadNotifications = await this._notificationService.GetUnreadNotificationsAsync(userId);
         return Ok(unreadNotifications.Select(notification => notification.ToDto()));
     }
 
     [HttpDelete("{notificationId:int}")]
     public async Task<IActionResult> RemoveNotification(int notificationId)
     {
-        await this.notificationService.RemoveNotificationAsync(notificationId);
+        await this._notificationService.RemoveNotificationAsync(notificationId);
         return Ok();
     }
 
     [HttpPost("{notificationId:int}/read")]
     public async Task<IActionResult> MarkRead(int notificationId)
     {
-        await this.notificationService.MarkReadAsync(notificationId);
+        await this._notificationService.MarkReadAsync(notificationId);
         return Ok();
     }
 
     [HttpPost("{notificationId:int}/mark-read-or-remove")]
     public async Task<IActionResult> MarkReadOrRemove(int notificationId)
     {
-        await this.notificationService.MarkAsReadOrRemoveAsync(notificationId);
+        await this._notificationService.MarkAsReadOrRemoveAsync(notificationId);
         return Ok();
     }
 
@@ -60,35 +60,35 @@ public sealed class NotificationsEndpointsController : ControllerBase
     [RequireMatchingUser]
     public async Task<IActionResult> MarkAllRead(int userId)
     {
-        await this.notificationService.MarkAllReadAsync(userId);
+        await this._notificationService.MarkAllReadAsync(userId);
         return Ok();
     }
 
     [HttpPost("generate/price-drop")]
     public async Task<IActionResult> GeneratePriceDropNotification([FromBody] GeneratePriceDropNotificationRequestBody requestBody)
     {
-        await this.notificationService.GeneratePriceDropNotificationAsync(requestBody.EventId, requestBody.EventTitle);
+        await this._notificationService.GeneratePriceDropNotificationAsync(requestBody.EventId, requestBody.EventTitle);
         return Ok();
     }
 
     [HttpPost("generate/seats-available")]
     public async Task<IActionResult> GenerateSeatsAvailableNotification([FromBody] GenerateSeatsAvailableNotificationRequestBody requestBody)
     {
-        await this.notificationService.GenerateSeatsAvailableNotificationAsync(requestBody.EventId, requestBody.EventTitle);
+        await this._notificationService.GenerateSeatsAvailableNotificationAsync(requestBody.EventId, requestBody.EventTitle);
         return Ok();
     }
 
     [HttpPost("notify/price-drop")]
     public async Task<IActionResult> NotifyPriceDrop([FromBody] NotifyPriceDropRequestBody requestBody)
     {
-        await this.notificationService.NotifyPriceDropAsync(requestBody.EventId, requestBody.OldPrice, requestBody.NewPrice);
+        await this._notificationService.NotifyPriceDropAsync(requestBody.EventId, requestBody.OldPrice, requestBody.NewPrice);
         return Ok();
     }
 
     [HttpPost("notify/seats-available")]
     public async Task<IActionResult> NotifySeatsAvailable([FromBody] NotifySeatsAvailableRequestBody requestBody)
     {
-        await this.notificationService.NotifySeatsAvailableAsync(requestBody.EventId, requestBody.NewCapacity);
+        await this._notificationService.NotifySeatsAvailableAsync(requestBody.EventId, requestBody.NewCapacity);
         return Ok();
     }
 }
