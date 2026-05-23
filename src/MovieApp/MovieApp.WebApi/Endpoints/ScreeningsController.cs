e610a5728790abfe6cb2f42cbf00cd4a3dd803a4
TaschinaLuca <taschina529@gmail.com>
2026-05-23 13:28:22 +0300

    1.22

--- src/MovieApp/MovieApp.WebApi/Endpoints/ScreeningsController.cs
+++ src/MovieApp/MovieApp.WebApi/Endpoints/ScreeningsController.cs
@@ -5,6 +5,7 @@ using MovieApp.WebDTOs.DTOs.RequestDTOs;
 
 namespace MovieApp.WebApi.Endpoints;
 
+[Authorize]
 [ApiController]
 [Route("api/screenings")]
 public sealed class ScreeningsController : ControllerBase
