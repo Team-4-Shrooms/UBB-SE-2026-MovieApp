a4f5e6cb66b747749ad8f2f2b14109f1ac571f7e
Andrei-Ilie Mateian <mailandrei2014@gmail.com>
2026-05-24 13:30:27 +0300

    refactored ct

--- src/MovieApp/MovieApp.Logic/Interfaces/Services/IBattleService.cs
+++ src/MovieApp/MovieApp.Logic/Interfaces/Services/IBattleService.cs
@@ -8,15 +8,15 @@ public interface IBattleService
 {
     Task<IEnumerable<Battle>> GetBattlesAsync(CancellationToken cancellationToken = default);
     Task<Battle?> GetBattleByIdAsync(int id, CancellationToken cancellationToken = default);
-    Task<Battle?> GetActiveBattleAsync(CancellationToken ct = default);
-    Task<Battle> CreateBattleAsync(int firstMovieId, int secondMovieId, CancellationToken ct = default);
-    Task<BattleBet> PlaceBetAsync(int userId, int battleId, int movieId, int amount, CancellationToken ct = default);
-    Task<BattleBet?> GetBetAsync(int userId, int battleId, CancellationToken ct = default);
-    Task<int> DetermineWinnerAsync(int battleId, CancellationToken ct = default);
-    Task DistributePayoutsAsync(int battleId, CancellationToken ct = default);
-    Task SettleExpiredBattlesAsync(CancellationToken ct = default);
-    Task<Battle?> GetCurrentBattleForUserAsync(int userId, CancellationToken ct = default);
-    Task ForceSettleBattleAsync(int battleId, CancellationToken ct = default);
-    Task ResetAllBattlesForDemoAsync(CancellationToken ct = default);
-    Task<Battle> CreateDemoBattleAsync(CancellationToken ct = default);
+    Task<Battle?> GetActiveBattleAsync(CancellationToken cancellationToken = default);
+    Task<Battle> CreateBattleAsync(int firstMovieId, int secondMovieId, CancellationToken cancellationToken = default);
+    Task<BattleBet> PlaceBetAsync(int userId, int battleId, int movieId, int amount, CancellationToken cancellationToken = default);
+    Task<BattleBet?> GetBetAsync(int userId, int battleId, CancellationToken cancellationToken = default);
+    Task<int> DetermineWinnerAsync(int battleId, CancellationToken cancellationToken = default);
+    Task DistributePayoutsAsync(int battleId, CancellationToken cancellationToken = default);
+    Task SettleExpiredBattlesAsync(CancellationToken cancellationToken = default);
+    Task<Battle?> GetCurrentBattleForUserAsync(int userId, CancellationToken cancellationToken = default);
+    Task ForceSettleBattleAsync(int battleId, CancellationToken cancellationToken = default);
+    Task ResetAllBattlesForDemoAsync(CancellationToken cancellationToken = default);
+    Task<Battle> CreateDemoBattleAsync(CancellationToken cancellationToken = default);
 }
