33fb5b9e19cba7478597da8b4e76643a0b4c9807
dariusturcu22 <protdm22@gmail.com>
2026-05-26 15:58:17 +0300

    Register ITriviaService proxy in Desktop DI

--- src/MovieApp/MovieApp/App.xaml.cs
+++ src/MovieApp/MovieApp/App.xaml.cs
@@ -98,6 +98,7 @@ namespace MovieApp
             services.AddTransient<ICommentService, CommentProxyService>();
             services.AddTransient<IAmbassadorService, AmbassadorProxyService>();
             services.AddTransient<IMarathonService, MarathonProxyService>();
+            services.AddTransient<ITriviaService, TriviaProxyService>();
 
             // Reels Upload
             services.AddTransient<MovieApp.Logic.Features.ReelsUpload.IVideoStorageService, VideoStorageProxyService>();
